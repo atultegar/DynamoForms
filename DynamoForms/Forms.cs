7c2970a1c278c1db87bba5e325764558b599fddf
Atul Tegar <atul.tegar@gmail.com>
2020-07-20 16:36:58 +0200

    updated with selectionlist

--- DynamoForms/Forms.cs
+++ DynamoForms/Forms.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Autodesk.DesignScript.Geometry;
 
+
 namespace DynamoForms
 {
     /// <summary>
@@ -56,6 +57,35 @@ namespace DynamoForms
             IList<object> data = (IList<object>)window.lstBox.SelectedItems;
             
             return data;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="title"></param>
+        /// <param name="list"></param>
+        /// <returns></returns>
+        [STAThread]
+        public static IList<object> FormListBox(String title, List<object> list)
+        {
+            IList<object> output = new List<object>();
+            SelectionList selectionList = new SelectionList();
+            selectionList.tb_1.Text = string.Format("Select {0} from list box", title);
+            for (int i=0; i < list.Count; i++)
+            {
+                selectionList.lstUnselected.Items.Add(list[i]);
+            }
+
+            var res = selectionList.ShowDialog();
+            if (!res.HasValue && res.Value)
+                selectionList.Close();
+            
+            for (int j=0; j < selectionList.lstSelected.Items.Count; j++)
+            {
+                output.Add(selectionList.lstSelected.Items[j]);
+            }
+
+            return output;
         }
     }
 }
